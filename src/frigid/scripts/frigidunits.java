abd1bd673306f3cfed525a75f573af35088cd11a
Hexazeta <124427272+Hexazeta@users.noreply.github.com>
2024-04-30 11:33:22 +0700

    fixingg

--- src/frigid/scripts/frigidunits.java
+++ src/frigid/scripts/frigidunits.java
@@ -79,7 +79,6 @@ public class frigidunits {
                     }};
 
                     sprite = "circle";
-                }};
-            }});
+                }});
+            }};
         }};
-}
