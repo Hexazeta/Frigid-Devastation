acf95c27c704f5851dd9da038d7b7191c101bfe2
Hexazeta <124427272+Hexazeta@users.noreply.github.com>
2024-04-30 19:17:24 +0700

    Update frigidunits.java

--- src/frigid/scripts/frigidunits.java
+++ src/frigid/scripts/frigidunits.java
@@ -48,7 +48,7 @@ public class frigidunits {
 
             outlineColor = Color.valueOf("2b2626");
 
-            weapons.add(new Weapon(NAME +"-ballistic-cannon"){{
+            weapons.add(new Weapon("frigid-ballistic-cannon"){{
                 x = 10;
                 y = 4;
                 alternate = true;
