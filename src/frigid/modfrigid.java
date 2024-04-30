62356ed158c7f9599db3089761260a4684238076
Hexazeta <124427272+Hexazeta@users.noreply.github.com>
2024-04-30 12:34:09 +0700

    wwawfawfawfawfeafaa

--- src/frigid/modfrigid.java
+++ src/frigid/modfrigid.java
@@ -2,10 +2,12 @@ package frigid;
 
 import mindustry.mod.*;
 import frigid.gen.*;
+import frigidunits;
 
 public class modfrigid extends Mod{
     @Override
     public void loadContent(){
         EntityRegistry.register();
+        frigidunits.load();
     }
 }
