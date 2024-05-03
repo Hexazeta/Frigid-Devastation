ed53822ef100d4fa0edb0d4deef05ed4b6583b9f
Hexazeta <124427272+Hexazeta@users.noreply.github.com>
2024-05-03 19:16:20 +0700

    fix

--- src/frigid/scripts/frigiditems.java
+++ src/frigid/scripts/frigiditems.java
@@ -4,27 +4,27 @@ public class frigiditems{
 
     public static void load() {
 
-        citrine = item item(name:"citrine");
+        citrine = new item(name:"citrine");
         citrine.color = pal2.citrine;
 
-        carborundum = item item(name:"carborundum");
+        carborundum = new item(name:"carborundum");
         carborundum.color = pal2.carborundum;
 
-        cobalt = item item(name:"cobalt");
+        cobalt = new item(name:"cobalt");
         cobalt.color = pal2.cobalt;
 
-        lithium = item item(name:"lithium");
+        lithium = new item(name:"lithium");
         lithium.color = pal2.lithium;
 
-        kyanite = item item(name:"kyanite");
+        kyanite = new item(name:"kyanite");
         kyanite.color = pal2.kyanite;
 
-        duralumin = item item(name:"duralumin");
+        duralumin = new item(name:"duralumin");
         duralumin.color = pal2.duralumin;
 
-        manganese = item item(name:"manganese");
+        manganese = new item(name:"manganese");
         manganese.color = pal2.manganese;
-        
+
     }
 
 
