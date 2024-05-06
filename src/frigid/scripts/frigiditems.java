1ea9ab9fad66f70aa112f1782bb6d4e06088326f
Hexazeta <124427272+Hexazeta@users.noreply.github.com>
2024-05-06 20:33:39 +0700

    color

--- src/frigid/scripts/frigiditems.java
+++ src/frigid/scripts/frigiditems.java
@@ -1,5 +1,6 @@
 package frigid.scripts;
 
+import arc.graphics.Color;
 import mindustry.type.Item;
 
 public class frigiditems {
@@ -8,17 +9,31 @@ public class frigiditems {
 
     public static void load() {
 
-        citrine = new Item("citrine");
+        citrine = new Item("citrine"){{
+            color = Color.valueOf("a34f05");
+        }};
 
-        carborundum = new Item("carborundum");
+        carborundum = new Item("carborundum"){{
+            color = Color.valueOf("122a82");
+        }};
 
-        cobalt = new Item("cobalt");
+        cobalt = new Item("cobalt"){{
+            color = Color.valueOf("4262d6");
+        }};
 
-        lithium = new Item("lithium");
+        lithium = new Item("lithium"){{
+            color = Color.valueOf("a39955");
+        }};
 
-        kyanite = new Item("kyanite");
+        kyanite = new Item("kyanite"){{
+            color = Color.valueOf("7bbfe0");
+        }};
 
-        duralumin = new Item("duralumin");
+        duralumin = new Item("duralumin"){{
+            color = Color.valueOf("6d7a87");
+        }};
 
-        manganese = new Item("manganese");
+        manganese = new Item("manganese"){{
+            color = Color.valueOf("570034");
+        }};
     }}
