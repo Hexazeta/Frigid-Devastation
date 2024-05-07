1a864c86c7310d1eb51b340d7c1d623107e36e22
Hexazeta <124427272+Hexazeta@users.noreply.github.com>
2024-05-07 19:00:00 +0700

    Update frigidblocks.java

--- src/frigid/scripts/frigidblocks.java
+++ src/frigid/scripts/frigidblocks.java
@@ -16,17 +16,17 @@ public class frigidblocks {
     public static void load(){
 
         carborundumCompressor = new GenericCrafter("carborundum-compressor"){{
-            requirements(Category.crafting, with(Items.copper, 30, Items.lead, 25));
+            requirements(Category.crafting, with(frigiditems.duralumin, 30, Items.graphite, 25));
             craftEffect = Fx.smeltsmoke;
-            outputItem = new ItemStack(Items.silicon, 1);
+            outputItem = new ItemStack(frigiditems.carborundum, 2);
             craftTime = 40f;
-            size = 2;
+            size = 3;
             hasPower = true;
             hasLiquids = false;
             ambientSound = Sounds.smelter;
             ambientSoundVolume = 0.07f;
 
-            consumeItems(with(frigiditems.citrine, 1, Items.graphite, 2));
+            consumeItems(with(frigiditems.citrine, 3, Items.graphite, 2));
             consumePower(0.50f);
     }};
 
