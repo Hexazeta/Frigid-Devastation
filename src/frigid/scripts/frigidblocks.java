0fe86ddb01c4b9e1f6d2f34ceb4e8ef64d36ad2e
Hexazeta <124427272+Hexazeta@users.noreply.github.com>
2024-05-07 19:18:44 +0700

    more blockssssssssssss

--- src/frigid/scripts/frigidblocks.java
+++ src/frigid/scripts/frigidblocks.java
@@ -2,6 +2,7 @@ package frigid.scripts;
 
 import mindustry.content.Fx;
 import mindustry.content.Items;
+import mindustry.content.Liquids;
 import mindustry.gen.*;
 import mindustry.type.*;
 import mindustry.world.*;
@@ -11,12 +12,13 @@ import static mindustry.type.ItemStack.*;
 public class frigidblocks {
 
     public static Block
-        carborundumCompressor,kyanitePress,duraluminSmelter;
+        carborundumCompressor,kyanitePress,duraluminSmelter,metalAmalgamate;
 
     public static void load(){
 
         carborundumCompressor = new GenericCrafter("carborundum-compressor"){{
-            requirements(Category.crafting, with(frigiditems.duralumin, 30, Items.graphite, 25));
+            requirements(Category.crafting,
+                    with(frigiditems.duralumin, 30, frigiditems.citrine, 25));
             craftEffect = Fx.smeltsmoke;
             outputItem = new ItemStack(frigiditems.carborundum, 2);
             craftTime = 40f;
@@ -28,7 +30,40 @@ public class frigidblocks {
 
             consumeItems(with(frigiditems.citrine, 3, Items.graphite, 2));
             consumePower(0.50f);
-    }};
+        }};
 
+        kyanitePress = new GenericCrafter("kyanite-press"){{
+            requirements(Category.crafting,
+                    with(frigiditems.duralumin, 30, Items.graphite, 25));
+            craftEffect = Fx.smeltsmoke;
+            outputItem = new ItemStack(frigiditems.kyanite, 2);
+            craftTime = 40f;
+            size = 3;
+            hasPower = true;
+            hasLiquids = false;
+            ambientSound = Sounds.smelter;
+            ambientSoundVolume = 0.07f;
+
+            consumeItems(with(frigiditems.citrine, 3, frigiditems.cryolite, 2));
+            consumePower(0.50f);
+        }};
+
+        metalAmalgamate = new GenericCrafter("metal-amalgamate"){{
+            requirements(Category.crafting,
+                    with(Items.graphite, 1));
+            craftEffect = Fx.smeltsmoke;
+            outputItem = new ItemStack(Items.copper, 55);
+            craftTime = 80f;
+            size = 3;
+            hasPower = true;
+            hasLiquids = true;
+            ambientSound = Sounds.smelter;
+            ambientSoundVolume = 0.07f;
+
+            consumeItems(with(frigiditems.citrine, 5, frigiditems.cryolite, 5,
+                    frigiditems.manganese, 4, frigiditems.cobalt, 4, Items.thorium, 3,
+                    frigiditems.lithium, 4, Items.graphite, 5, Liquids.water, 120));
+            consumePower(10f);
+        }};
     }
 }
