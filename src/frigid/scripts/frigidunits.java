0ce58b7118519b4ebf88d5a1e8c2237dac465080
Hexazeta <124427272+Hexazeta@users.noreply.github.com>
2024-05-09 12:05:56 +0700

    aeaeacleanup

--- src/frigid/scripts/frigidunits.java
+++ src/frigid/scripts/frigidunits.java
@@ -1,21 +1,9 @@
 package frigid.scripts;
 
-import static mindustry.Vars.*;
 import mindustry.*;
 import arc.graphics.Color;
-import arc.math.Interp;
-import arc.math.Mathf;
-import arc.util.Time;
-import mindustry.ai.types.BuilderAI;
-import mindustry.ai.types.CargoAI;
 import mindustry.ai.types.GroundAI;
-import mindustry.content.Fx;
 import mindustry.entities.bullet.*;
-import mindustry.entities.effect.ParticleEffect;
-import mindustry.entities.part.HaloPart;
-import mindustry.entities.part.RegionPart;
-import mindustry.entities.pattern.ShootHelix;
-import mindustry.entities.pattern.ShootSpread;
 import mindustry.gen.*;
 import mindustry.graphics.Layer;
 import mindustry.type.UnitType;
@@ -29,7 +17,6 @@ public class frigidunits {
             
             //spiker kaboom
             spiker;
-    ;
 
     public static void load() { 
 
@@ -37,12 +24,12 @@ public class frigidunits {
             constructor = MechUnit::create;
             aiController = GroundAI::new;
 
-            speed = 0.25f;
+            speed = 0.75f;
             drag = 0.11f;
             hitSize = 24f;
             rotateSpeed = 3f;
             health = 5040;
-            armor = 24f;
+            armor = 6f;
 
             shadowElevation = 0.1f;
             groundLayer = Layer.legUnit - 1f;
@@ -51,16 +38,20 @@ public class frigidunits {
 
             outlineColor = Color.valueOf("2b2626");
 
-            weapons.add(new Weapon("Frigid-ballistic-cannon"){{
+            weapons.add(new Weapon("ballistic-cannon"){{
+
                 x = 10;
                 y = 4;
+                shoot.shots = 3;
+                range = 25;
                 alternate = true;
                 mirror = true;
                 reload = 24f;
-                inaccuracy = 1f;
+                inaccuracy = 10f;
 
                 shootSound = Vars.tree.loadSound("pewpewpew");
                 bullet = new BasicBulletType(6, 120){{
+                    fragBullets = 5;
                     width = 11;
                     height = 19;
                     }};
@@ -97,4 +88,4 @@ public class frigidunits {
             outlineColor = Color.valueOf("2b2626");
 
         }};
-    }};
+    }}
